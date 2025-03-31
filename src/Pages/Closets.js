cbc6b1de5fbe974b71ace74f178c26810de171dc
unknown <alannaphung816@gmail.com>
2025-03-30 21:49:10 -0500

    created card component for closets, made JSON

--- src/Pages/Closets.js
+++ src/Pages/Closets.js
@@ -1,12 +1,35 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+
+import closetsData from "../closets.json";
 
 function Closets() {
   return (
     <div className="page">
       <h2>Closets</h2>
-      <p>closet cars here</p>
+      <div>
+      {closetsData.closets.map((closet) => (
+        <ClosetCard
+          title={closet.title}
+          members={closet.num_members}
+          items={closet.num_items}
+          status={closet.status}
+        />
+      ))}
+      </div>
     </div>
   );
 }
 
+const ClosetCard = ({ title, members, items, status}) => {
+  return (
+    <Card style={{ margin: '10px' }}>
+      <Card.Body className="closetcard">
+        <Card.Title>{title}, {status}</Card.Title>
+        <Card.Text>{members} members </Card.Text>
+        <Card.Text>{items} items </Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
 export default Closets;
