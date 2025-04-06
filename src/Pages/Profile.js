6cd4fb364759c03aff19e9e585748f1a903f9e11
Victoria Ocando <victoriaocando03@gmail.com>
2025-04-06 17:58:21 -0500

    outfits synced to profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -40,6 +40,27 @@ const GetUsername = () => {
 };
 
 function Profile() {
+
+  // state for outfits/closets in local storage
+  const [outfits, setOutfits] = useState([]);
+  const [closets, setClosets] = useState([]);
+
+  useEffect(() => {
+    // load outfits/closets from local storage
+    const storedOutfits = localStorage.getItem('outfits');
+    if (storedOutfits) {
+      setOutfits(JSON.parse(storedOutfits));
+    }
+    const storedClosets = localStorage.getItem('closets');
+    if (storedClosets) {
+      setClosets(JSON.parse(storedClosets));
+    }
+  }, []);
+
+  // profile now updates user's number of closets/outfits
+  const numClosets = closets.length;
+  const numOutfits = outfits.length;
+
   return (
     <div className="profilepage">
       <ScreenHeader text="Profile" />
@@ -51,79 +72,35 @@ function Profile() {
             <GetUsername></GetUsername>
           <div className="row">
             <div className="col-4">
-              <p>3 Closets</p>
+              <p>{numClosets} Closets</p>
             </div>
             <div className="col-4">
-              <p>1 Outfit</p>
+              <p>{numOutfits} Outfits</p>
             </div>
           </div>
           </div>
         </div>
         <div className="outfitsContainer">
           <h1>My Outfits</h1>
+          {outfits.map((outfit) => (
+          <div className="outfitContainer" key={outfit.id}>
+            <p>{outfit.name}</p>
+            <div className="outfitImages">
+              {outfit.items.map((item, index) => (
+                <img
+                  key={index}
+                  src={item.image}
+                  alt={item.name}
+                  className="itemImage"
+                />
+              ))}
+            </div>
+          </div>
+        ))}
         </div>
-        <div className="outfitContainer">
-          <p>Career Fair</p>
-          <div>
-          <img src={`${process.env.PUBLIC_URL}/images/items/button_up.jpg`} alt="white button-up shirt" className="itemImage"></img>
-          <img src={`${process.env.PUBLIC_URL}/images/items/slacks.jpg`} alt="black dress pants" className="itemImage"></img>
-          <img src={`${process.env.PUBLIC_URL}/images/items/blazer.jpg`} alt="black buttoned blazer" className="itemImage"></img>
-          <img src={`${process.env.PUBLIC_URL}/images/items/heels.jpg`} alt="black heels" className="itemImage"></img>
-          </div>
-              
-          </div>
-      
-        
-
       </div>
       
   );
 }
 
 export default Profile;
-
-
-// div className="row userInfoContainer">
-//         <div className="col-5"> {/* column for pfp */}
-//             <img src={`${process.env.PUBLIC_URL}/images/jenna_pfp.jpg`} alt="Jenna's profile" className="profilePicture" />
-//         </div>
-//         <div className="col-7"> {/* column for user info */}
-//             <div className="row">
-//                 <h1>Jenna Doe</h1>
-//                 <p>@jennadoe123</p>
-//             </div>
-//             <div className="row">
-//                 <div className="col-4">
-//                     <p>3 Closets</p>
-//                 </div>
-//                 <div className="col-4">
-//                     <p>1 Outfit</p>
-//                 </div>
-//             </div>
-            
-//         </div>
-//       </div>
-
-//       <div className="row outfitsContainer">
-//           <div className="row">
-//               <h1>My Outfits</h1>
-//           </div>
-//           <div className="row outfitContainer">
-//               <p>Career Fair</p>
-//               <div className="row">
-//                   <div className="col-3">
-//                       <img src={`${process.env.PUBLIC_URL}/images/items/button_up.jpg`} alt="white button-up shirt" className="itemImage"></img>
-//                   </div>
-//                   <div className="col-3">
-//                       <img src={`${process.env.PUBLIC_URL}/images/items/slacks.jpg`} alt="black dress pants" className="itemImage"></img>
-//                   </div>
-//                   <div className="col-3">
-//                       <img src={`${process.env.PUBLIC_URL}/images/items/blazer.jpg`} alt="black buttoned blazer" className="itemImage"></img>
-//                   </div>
-//                   <div className="col-3">
-//                       <img src={`${process.env.PUBLIC_URL}/images/items/heels.jpg`} alt="black heels" className="itemImage"></img>
-//                   </div>
-//               </div>
-//           </div>
-          
-//       </div>
